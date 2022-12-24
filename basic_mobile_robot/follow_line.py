cac6e23b5b6580aa560548aa841f9785e2692c86
robo <robo2020@gmail.com>
2022-12-24 07:51:16 +0200

    follow_the_line_basic

---
+++ basic_mobile_robot/follow_line.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+import rclpy
+import cv2
+import numpy as np
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import Twist
+
+CAMERA_TOPIC = "/camera/image_raw"
+TWIST_TOPIC = "/cmd_vel"
+
+class ImageViewer(Node):
+    def __init__(self):
+        super().__init__("viewer")
+        self.image_sub = self.create_subscription(Image, CAMERA_TOPIC, self.callback, 10)
+        self.twist_pub = self.create_publisher(Twist, TWIST_TOPIC, 10)
+        self.bridge = CvBridge()
+
+    def find_line(self, frame):
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_yellow = np.array([ 10,  10,  10])
+        upper_yellow = np.array([255, 255, 250])
+        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        
+        # BEGIN CROP
+        h, w, d = frame.shape
+        search_top = int(3*h/4)
+        search_bot = int(search_top + 60)
+        mask[0:search_top, 0:w] = 0
+        mask[search_bot:h, 0:w] = 0
+        # END CROP
+        # BEGIN FINDER
+        M = cv2.moments(mask)
+        if M['m00'] > 0:
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+        # END FINDER
+        # BEGIN CIRCLE
+        return cx, cy
+        
+        # END CIRCLE
+
+    def callback(self,data):
+        try:
+            frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            cx, cy = self.find_line(frame)
+            cv2.circle(frame, (cx, cy), 20, (0,0,255), -1)
+            h, w, d = frame.shape
+            center = int(w/2)
+            x = 0.0
+            if cx < center - 10:
+                z = 0.1
+            elif cx > center + 10:
+                z = -0.1
+            else:
+                z = 0.0
+                x = 0.2
+
+            msg = Twist()
+            msg.linear.x = x
+            msg.angular.z = z
+            self.twist_pub.publish(msg)
+
+
+        except CvBridgeError as e:
+            print(e)
+
+        cv2.imshow("image", frame)
+        cv2.waitKey(3)
+
+def main(args=None):
+    try:
+        rclpy.init()
+        image_converter = ImageViewer()
+        rclpy.spin(image_converter)
+        image_converter.destroy_node
+        rclpy.shutdown()
+    except KeyboardInterrupt:
+        print("Shutting down")
+        cv2.destroyAllWindows()
+if __name__ == '__main__':   
+    main()
---
+++ launch/follow/follow.launch.py
@@ -0,0 +1,56 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration, Command
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, AppendEnvironmentVariable
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import xacro
+
+PACKAGE = "basic_mobile_robot"
+WORLD = "course.world"
+SDF = 'robot.urdf.xacro'
+
+def generate_launch_description():
+    pkg_gazebo_ros = get_package_share_directory("gazebo_ros")
+    pkg = get_package_share_directory(PACKAGE)
+
+    resources = [os.path.join(pkg, "worlds")]
+
+    resource_env = AppendEnvironmentVariable(
+        name="GAZEBO_RESOURCE_PATH", value=":".join(resources)
+    )
+
+    models = [os.path.join(pkg, "models")]
+
+    models_env = AppendEnvironmentVariable(
+        name="GAZEBO_MODEL_PATH", value=":".join(models)
+    )
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    pkg,'launch',"articubot_one", 'rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'false'}.items()
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, "launch", "gazebo.launch.py"),
+        ),
+        launch_arguments={"verbose": "true", "world": WORLD}.items(),
+    )
+
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-entity", "demo", "-topic", "robot_description"],
+        output="screen",
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(rsp)
+    ld.add_action(models_env)
+    ld.add_action(resource_env)
+    ld.add_action(gazebo)
+    ld.add_action(spawn_entity)
+    return ld
